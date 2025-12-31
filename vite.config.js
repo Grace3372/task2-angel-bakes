462d293449668630a2743d12fc3a8229010d43fe
Grace3372 <pasumarthigrace@gmail.com>
2025-12-31 18:35:35 +0530

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from "vite";
+import nunjucks from "vite-plugin-nunjucks";
+
+export default defineConfig({
+  plugins: [
+    nunjucks({
+      templatesDir: "src/templates"
+    })
+  ],
+  build: {
+    outDir: "dist",
+    emptyOutDir: true
+  }
+});
